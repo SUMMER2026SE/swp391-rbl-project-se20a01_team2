50aa0cba62a70963468ac05eea0085b4ab6b3dc4
Le Huy Manh Tan <81731577+lehuymanhtan@users.noreply.github.com>
2026-07-04 22:24:15 +0700

    cleanup repository

--- IELTSFLOW/db_migration_chunked_upload.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE upload_sessions (
-    upload_id VARCHAR(255) PRIMARY KEY,
-    file_name VARCHAR(255) NOT NULL,
-    total_chunks INT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
